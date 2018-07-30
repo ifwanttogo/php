76e4261f0ec5b680eda0d0783bd4eece9aa9fd21
吴云飞 <771687865@qq.com>
2018-07-30 17:47:09 +0800

    php 数组小结

---
+++ manual/syntax.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaohan
+ * Date: 2018-07-27
+ * Time: 11:07
+ */
+
+/**
+ * 默认语法
+ * 默认界定的的语法以<?php开头,以?>结束，如下：
+*/
+?>
+<?php echo  "<p> some dynamic output here</p>";?>
+<?php
+//returns some dynamic output here
+
+/*
+ * 短标签
+ */
+?>
+<?
+print "this is another php example";
+?>
+<?php
+//returns this is another php example
+
+/*
+ * 脚本
+ * <script language = "php">
+    print "this is another php example";
+   </script>
+ *
+ */
+
+/*
+ * ASP风格
+ * <%
+    print "this is another php example";
+    %>
+ */
+
+
+/*
+ * print() 语句
+ * print() 语句吧
+ */
+
+
+?>
+
+
+
+
+
+
